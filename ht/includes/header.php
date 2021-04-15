546cbf0d4018da5b8f6562a57651ffbdb20cf3a3
Yaswanth8021 <62641647+Yaswanth8021@users.noreply.github.com>
2021-04-15 09:39:38 +0530

    Update header.php

--- ht/includes/header.php
+++ ht/includes/header.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Hotel & Tourism</title>
+    <title>HotelManagement-System</title>
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/w3.css" rel="stylesheet">
